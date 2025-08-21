ab9157547f88803a54f7e15311a54c4a8cbdf88c
dennis-gr <dennis-gr@users.noreply.github.com>
2025-08-21 13:13:53 +0200

    Add VauHttpClientHandler for transparent encryption and decryption f VAU requests

--- lib-vau-csharp-test/HandShakeTests.cs
+++ lib-vau-csharp-test/HandShakeTests.cs
@@ -12,18 +12,16 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * For additional notes and disclaimer from gematik and in case of changes by gematik find details in the "Readme" file.
  */
 
 using lib_vau_csharp;
 using lib_vau_csharp.data;
 
 using NUnit.Framework;
-using NUnit.Framework.Legacy;
 
 using System;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace lib_vau_csharp_test
 {
@@ -52,11 +50,10 @@ namespace lib_vau_csharp_test
 
             vauServer = new VauServer(url, signedPublicVauKeys, Constants.Keys.EccKyberKeyPair);
             _ = vauServer.StartAsync();
-
         }
 
         [TearDown]
-        public static void ShutdownServer()
+        public void ShutdownServer()
         {
             if (vauServer != null)
             {
@@ -65,14 +62,15 @@ namespace lib_vau_csharp_test
         }
 
         [Test]
-        public static void TestSendingMessagesThroughChannel()
+        public async Task TestSendingMessagesThroughChannel()
         {
-            vauClient = new VauClient();
-            bool handshakeSucceeded = vauClient.DoHandshake(url).Result;
+            var httpClient = new HttpClient { BaseAddress = new Uri(url) };
+            var vauClient = new VauClient(httpClient);
+            await vauClient.DoHandshake();
 
-            ClassicAssert.IsTrue(handshakeSucceeded);
-            bool messagesExchanged = vauClient.SendMessage(url, Encoding.UTF8.GetBytes("Hello World!")).Result;
-            ClassicAssert.IsTrue(messagesExchanged);
+            var replyMessage = await vauClient.SendMessage("Hello World!"u8.ToArray());
+            Assert.That(replyMessage.Content, Is.Not.Null);
+            Console.WriteLine(await replyMessage.Content.ReadAsStringAsync());
         }
     }
 }
